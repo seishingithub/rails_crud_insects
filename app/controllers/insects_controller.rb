dd6e012f2a5ef0055494fa4070a05183f8a7c349
Peggy Griffin <seishingithub@gmail.com>
2014-06-17 23:34:10 -0600

    User can edit and update list of insects

--- app/controllers/insects_controller.rb
+++ app/controllers/insects_controller.rb
@@ -14,6 +14,21 @@ class InsectsController < ApplicationController
     end
   end
 
+  def show
+    @insect = Insect.find(params[:id])
+  end
+
+  def edit
+    @insect = Insect.find(params[:id])
+  end
+
+  def update
+    @insect = Insect.find(params[:id])
+    @insect.update_attributes!(insect_params)
+
+    redirect_to insects_path
+  end
+
   private
   def insect_params
     params.require(:insect).permit(:name, :characteristic)
--- spec/features/insects_spec.rb
+++ spec/features/insects_spec.rb
@@ -7,8 +7,29 @@ feature 'Manage Insects' do
     click_on 'Add an insect'
     fill_in 'Name', with: 'grasshopper'
     fill_in 'Characteristic', with: 'jumps'
-    click_on 'Create   insect'
+    click_on 'Create insect'
     expect(page).to have_content 'grasshopper'
     expect(page).to have_content 'jumps'
+  end
+
+  scenario 'User can edit and update list of insects' do
+    visit '/'
+    click_on 'Add an insect'
+    fill_in 'Name', with: 'grasshopper'
+    fill_in 'Characteristic', with: 'jumps'
+    click_on 'Create insect'
+    expect(page).to have_content 'grasshopper'
+    expect(page).to have_content 'jumps'
+    click_on 'grasshopper'
+    expect(page).to have_content 'grasshopper'
+    expect(page).to have_content 'jumps'
+    click_on 'Edit insect'
+    fill_in 'Name', with: 'cockroach'
+    fill_in 'Characteristic', with: 'scary'
+    click_on 'Update insect'
+    expect(page).to have_no_content 'grasshopper'
+    expect(page).to have_no_content 'jumps'
+    expect(page).to have_content 'cockroach'
+    expect(page).to have_content 'scary'
   end
 end
